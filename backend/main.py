5205ece960d0d59e419e498aa8a278554c197a10
Kiruha01 <liss-2001g@mail.ru>
2021-11-01 01:24:45 +0300

    Delete page on '/'

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,9 @@ from flask import Flask
 from backend.core_api import api
 from flask_cors import CORS
 
+
 def create_app():
     app = Flask(__name__)
-    # доступ ко всем эндпоинтам??
     CORS(app)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
@@ -17,23 +17,6 @@ def create_app():
         response.headers["Access-Control-Allow-Headers"] = "Content-Type"
         response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
         return response
-
-    @app.route('/')
-    def hello():
-        return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="/api/report/" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-        <select name=type>
-            <option value=sun>ДС</option>
-            <option value=ultraviolet>УФ</option>
-
-        </select>
-         <input type=submit value=Upload>
-    </form>
-    '''
 
     app.register_blueprint(api, url_prefix='/api/')
 
