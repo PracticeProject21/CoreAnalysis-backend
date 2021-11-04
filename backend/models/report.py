9994f0a7a6e1737ad1c355c6324524e6b9b05902
Dmitry913 <lazer01@inbox.ru>
2021-11-04 22:39:06 +0300

    fsa

--- backend/models/report.py
+++ backend/models/report.py
@@ -5,3 +5,4 @@ class Report(db.Model):
     report_id = db.Column(db.Integer, primary_key=True)
     json_date = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    reports = db.relationship('Segment', backref='segment')
---
+++ backend/models/segment.py
@@ -0,0 +1,8 @@
+from backend.database import db
+
+class Segment(db.Model):
+    __tablename__ = 'segment'
+    segment_id = db.Column(db.Integer, primary_key=True)
+    segment_type = db.Column(db.String(30), nullable=False)
+    info = db.Column(db.String(255), nullable=False)
+    report_id = db.Column(db.Integer, db.ForeignKey('report_id'), nullable=False)
