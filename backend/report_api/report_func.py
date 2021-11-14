fdcc88ce69c91a7ca4b7659ceac12c4d5e58302e
Kiruha01 <liss-2001g@mail.ru>
2021-11-14 14:51:08 +0300

    Add functions to represent report

--- backend/report_api/report_func.py
+++ backend/report_api/report_func.py
@@ -7,6 +7,8 @@ from backend.models.report import Report
 from backend.database import db
 from typing import Dict, List, AnyStr
 
+from backend.core_api.fields_control import config, delete_mentioned_property
+
 
 def validate_segment(json: dict) -> dict:
     """- Delete invalid fields """
@@ -25,3 +27,55 @@ def create_report(photo_name: AnyStr, photo_url: AnyStr, photo_type: AnyStr, seg
     for segment in segments:
         report.segments.append(create_segment(segment))
     return report
+
+
+def find_property(source, props):
+    """Поиск следующего свойства"""
+    for field in source:
+        try:
+            next(x for x in props.keys() if x == field['name'])
+        except StopIteration:
+            continue
+        else:
+            return field
+    return None
+
+
+def find_value(vals, val_name):
+    try:
+        found_val = next(x for x in vals if x['name'] == val_name)
+    except StopIteration:
+        return None
+    return found_val
+
+
+def convert_segment_to_readable_view(segment: Segment) -> List[Dict]:
+    info = json.loads(segment.info)
+    source = config
+    out_list = list()
+    next_sources = []
+    while True:
+        prop = find_property(source, info)
+        if prop:
+            val = find_value(prop['values'], info[prop['name']])
+            if val:
+                out_list.append({'name': prop['name'], 'title': prop['title'], "value": {
+                    'name': val['name'],
+                    'title': val['title']
+                }})
+                del info[prop['name']]
+                try:
+                    next_sources.append(val['nested'])
+                except KeyError:
+                    pass
+            else:
+                del info[prop['name']]
+
+        else:
+            try:
+                source = next_sources.pop(0)
+            except IndexError:
+                break
+    return out_list
+
+
--- tests/test_reportApi.py
+++ tests/test_reportApi.py
@@ -1,8 +1,10 @@
 from fixtures import client2
 import pytest
 
-from backend.report_api.report_func import create_segment
+from backend.report_api.report_func import create_segment, convert_segment_to_readable_view
 from backend.database import db
+
+from backend.models.segment import Segment
 
 
 @pytest.mark.parametrize("data", [
@@ -12,3 +14,8 @@ def test_create_segment(data, client2):
     segment = create_segment(data)
     assert segment.info == '{"photo_type": "sun", "sun_type": "destruction"}'
 
+
+def test_segment_representation():
+    seg = Segment(info='{"photo_type": "sun", "sun_type": "destruction", "test_fields": "test_1"}')
+    print(convert_segment_to_readable_view(seg))
+    assert convert_segment_to_readable_view(seg) is None
