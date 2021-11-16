3915ffe770052417f8c26ce51b4b32bec30de20f
Dmitry913 <st077178@student.spbu.ru>
2021-11-16 20:30:15 +0300

    Add methods for update and delete segments

--- backend/report_api/__init__.py
+++ backend/report_api/__init__.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+import json
 
 from backend.models.report import Report
 from .report_func import convert_report_to_json
 from backend.database import db
-
+from ..models.segment import Segment
 
 report = Blueprint('report_api', __name__)
 
@@ -27,3 +28,28 @@ def get_reports(report_id):
 def get_reports_by_id(report_id):
     db.session.query(Report).filter_by(report_id=report_id).delete()
     db.session.commit()
+
+
+@report.route('/segments/<int:segment_id>/', methods=['PUT', "PATCH"])
+def update_segment(segment_id):
+    Segment.query.get_or_404(segment_id)
+    props = {}
+    data = json.loads(request.data.decode())
+    if data.get('offset'):
+        props['offset'] = data.get('offset')
+    if data.get('properties'):
+        props['info'] = json.dumps(data.get('properties'))
+    print(props)
+
+    db.session.query(Segment).filter_by(segment_id=segment_id).update(props)
+    db.session.commit()
+    return '', 204
+
+
+@report.route('/segments/<int:segment_id>/', methods=['DELETE'])
+def delete_segment(segment_id):
+    Segment.query.get_or_404(segment_id)
+    db.session.query(Segment).filter_by(segment_id=segment_id).delete()
+    db.session.commit()
+    return '', 204
+
